8793b9d4155c3b29d2da94f3058310e3c33f018e
MrChampion91 <vav206g@gmail.com>
2022-10-11 21:00:00 +0300

    add color picker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 '''''''''
 app = QApplication(sys.argv) #create app
@@ -13,7 +14,6 @@ dlgMain.show()
 sys.exit(app.exec_())   #event loop
 '''''''''
 
-
 class DlgMain(QDialog):
     def __init__(self):
         super().__init__()
@@ -24,13 +24,25 @@ class DlgMain(QDialog):
         self.ledText = QLineEdit("award", self)
         self.ledText.move(80, 1)
 
-        # button
+        # button 1
         self.btnUpdate = QPushButton("press", self)
         # self.btnUpdate.move(80, 1)
-        self.btnUpdate.clicked.connect(self.updateButton)
+        self.btnUpdate.clicked.connect(self.updateButton1)
 
-    def updateButton(self):
-        self.setWindowTitle(self.ledText.text())
+        # button 2
+        self.btnUpdate = QPushButton("color", self)
+        self.btnUpdate.move(0, 25)
+        self.btnUpdate.clicked.connect(self.updateButton2)
+
+    def updateButton1(self):
+        res = QMessageBox.information(self, " ", self.ledText.text()) #warning information
+        #self.setWindowTitle(self.ledText.text())
+        print(res)
+
+    def updateButton2(self):
+        color = QColorDialog.getColor(QColor("red"), self, "choose Color")
+        #self.QColorDialog.setCurrentColor(color)
+        print(color)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)  # create app
