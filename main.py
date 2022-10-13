98032a0d71a025696ee8b0dbc7d70e52ea1220d7
MrChampion91 <vav206g@gmail.com>
2022-10-13 15:49:04 +0300

    add image

--- main.py
+++ main.py
@@ -18,30 +18,49 @@ class DlgMain(QDialog):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("main window")
-        self.resize(300, 400)
+        self.resize(500, 300)
+
+        #font
+        font = QFont("Times New Roman", 14, 75, True)
+        self.setFont(font)
+
+        #image
+        img = QPixmap("images.jpg")
+
 
         # text
         self.ledText = QLineEdit("award", self)
         self.ledText.move(80, 1)
 
+        #lable
+        self.lbl = QLabel("lable text", self)
+        self.lbl.move(60, 50)
+
+        self.lbl.setPixmap(img) # add image
+
         # button 1
-        self.btnUpdate = QPushButton("press", self)
-        # self.btnUpdate.move(80, 1)
-        self.btnUpdate.clicked.connect(self.updateButton1)
+        self.btn = QPushButton("press", self)
+        # self.btn.move(80, 1)
+        self.btn.clicked.connect(self.updateButton1)
 
         # button 2
-        self.btnUpdate = QPushButton("color", self)
-        self.btnUpdate.move(0, 25)
-        self.btnUpdate.clicked.connect(self.updateButton2)
+        self.btn2 = QPushButton("color", self)
+        self.btn2.move(0, 25)
+        self.btn2.clicked.connect(self.updateButton2)
 
     def updateButton1(self):
         res = QMessageBox.information(self, " ", self.ledText.text()) #warning information
+        font = QFontDialog.getFont()
+        self.lbl.setFont(font[0])
+        self.lbl.setText(self.ledText.text())
         #self.setWindowTitle(self.ledText.text())
         print(res)
+        print(font)
 
     def updateButton2(self):
         color = QColorDialog.getColor(QColor("red"), self, "choose Color")
         #self.QColorDialog.setCurrentColor(color)
+
         print(color)
 
 if __name__ == "__main__":
